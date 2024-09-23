cd6bc61282b20acba4853f6d38defa8d2021d876
Luis <luishenriquehleme15@gmail.com>
2024-09-23 17:02:11 -0300

    Controle-de-Fluxo

--- src/CaixaEletronica.java
+++ src/CaixaEletronica.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class CaixaEletronica {
+    public static void main(String[] args) {
+        // Condição Simples
+        double saldo = 25.0;
+        double valorSolicitado = 22.0;
+
+        if(valorSolicitado < saldo) {
+            saldo = saldo - valorSolicitado;
+            System.out.println("Novo saldo " + saldo);
+        }else
+            System.out.println("Saldo insuficiente");
+
+
+
+
+
+
+    }
 }
--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class ResultadoEscolar {
+    public static void main(String[] args) {
+
+        int nota = 10;
+        if(nota>= 7)
+            System.out.println("Aprovado");
+
+        else if (nota >= 5 && nota < 7) {
+        System.out.println("Prova de Recuperção");
+
+        }else
+            System.out.println("Reprovado");
+    }
 }
